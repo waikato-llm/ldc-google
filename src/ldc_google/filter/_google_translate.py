871b87ebe950c305ca316108b30d8475d69d5326
Peter Reutemann <fracpete@gmail.com>
2024-02-29 10:58:07 +1300

    centralized adding parser arg for locations in add_location_argument method

--- src/ldc_google/filter/_google_translate.py
+++ src/ldc_google/filter/_google_translate.py
@@ -1,16 +1,17 @@
 import argparse
 import copy
-from google.cloud import translate
 from typing import List, Union
 
+from google.cloud import translate
 from wai.logging import LOGGING_WARNING
-from ldc.core import DOMAIN_PAIRS, DOMAIN_PRETRAIN, DOMAIN_TRANSLATION
-from ldc.core import LOCATION_ANY, LOCATION_INSTRUCTION, LOCATION_INPUT, LOCATION_OUTPUT, LOCATION_CONTENT, \
-    LOCATIONS, LOCATIONS_PAIRS, LOCATIONS_PRETRAIN, LOCATIONS_TRANSLATION, locations_match
+
 from ldc.api import Filter
 from ldc.api.pretrain import PretrainData
 from ldc.api.supervised.pairs import PairData
 from ldc.api.translation import TranslationData
+from ldc.core import DOMAIN_PAIRS, DOMAIN_PRETRAIN, DOMAIN_TRANSLATION
+from ldc.core import LOCATION_ANY, LOCATION_INSTRUCTION, LOCATION_INPUT, LOCATION_OUTPUT, LOCATION_CONTENT, \
+    LOCATIONS, locations_match, add_location_argument
 from ldc.text_utils import remove_empty
 
 
@@ -120,7 +121,7 @@ class GoogleTranslate(Filter):
         parser.add_argument("-s", "--source_lang", type=str, default=None, help="The language the incoming text is in.", required=False)
         parser.add_argument("-t", "--target_lang", type=str, default=None, help="The language to translate the text into.", required=False)
         parser.add_argument("--split_lines", action="store_true", help="Whether to split the text on new lines rather than presenting it as a single item to translate.")
-        parser.add_argument("-L", "--location", choices=LOCATIONS, nargs="*", default=LOCATION_ANY, help="Which data use for counting tokens; pairs: " + ",".join(LOCATIONS_PAIRS) + ", pretrain: " + ",".join(LOCATIONS_PRETRAIN) + ", translation: " + ",".join(LOCATIONS_TRANSLATION))
+        add_location_argument(parser, "Which data use for counting tokens")
         return parser
 
     def _apply_args(self, ns: argparse.Namespace):
